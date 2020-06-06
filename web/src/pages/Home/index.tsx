0b9ba4238aa1ee312fc6dfc44ad3c5ef96c58abc
Iago Balbino <iago_nobre@hotmail.com>
2020-06-05 21:38:24 -0300

    Adicionado home e a página de cadastro de ponto de coleta

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css'
+import logo from '../../assets/logo.svg';
 
 const Home = () => {
     return (
-        <h1>Hello world</h1>
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta"/>
+                </header>
+
+                <main>
+                    <h1>Seu marktplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encotrarem pontos de coleta de forma efeiciete.</p>
+
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
+                </main>
+            </div>
+        </div>
     )
 }
 
